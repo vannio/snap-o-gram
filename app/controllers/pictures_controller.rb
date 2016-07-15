e298f6b788ba948a9c41e3a75bc6487eec4cf9f9
Van Le <singularity@vannio.co.uk>
2016-07-15 23:31:55 +0100

    App looks a bit more like instagram

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -4,7 +4,7 @@ class PicturesController < ApplicationController
   # GET /pictures
   # GET /pictures.json
   def index
-    @pictures = Picture.all
+    @pictures = Picture.all.order('created_at DESC')
   end
 
   # GET /pictures/1
