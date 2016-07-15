e1e671f5a0d56f96e963ff97296e3235e6e1d0a4
Van Le <singularity@vannio.co.uk>
2016-07-15 18:17:25 +0100

    Fix img upload spec, add carrierwave gem

--- spec/features/upload_img_spec.rb
+++ spec/features/upload_img_spec.rb
@@ -1,8 +1,8 @@
 feature 'Upload Pictures' do
   scenario 'User can upload picture' do
     visit '/pictures/new'
-    attach_file('Image', '/assets/test_1.jpg')
-    click_button :'Create picture'
-    expect(page).to have_content('img')
+    attach_file('picture[file_path]', './spec/assets/test_1.jpg')
+    click_button :'Create Picture'
+    expect(page).to have_content('Picture was successfully created.')
   end
 end
