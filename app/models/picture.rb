53e64ba4d54b442934e968b81239038d223faadd
Van Le <singularity@vannio.co.uk>
2016-07-16 14:00:03 +0100

    Add comments model and migrations

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,5 +1,7 @@
 class Picture < ApplicationRecord
   require 'carrierwave/orm/activerecord'
   belongs_to :user
+  has_many :comments, dependent: :destroy
+  
   mount_uploader :file_path, PictureUploader
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  root to: 'pictures#index'
 
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
-  root to: 'pictures#index'
-  resources :pictures
+
+  resources :pictures do
+    resources :comments
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
