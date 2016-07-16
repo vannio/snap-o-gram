00c9a21ed52cb007174e879dc1a3914bb0b13280
Van Le <singularity@vannio.co.uk>
2016-07-16 15:16:30 +0100

    Add test for editing comments

--- spec/features/picture_comments_spec.rb
+++ spec/features/picture_comments_spec.rb
@@ -1,11 +1,23 @@
 feature 'Picture Comments' do
-  scenario 'User can add a comment to a photo' do
+  before do
     sign_up
     upload_photo
     click_link(:Show)
     fill_in(:'comment[text]', with: 'Hello, this is comment')
     click_button(:'Add Comment')
     click_link(:Show)
+  end
+
+  scenario 'User can add a comment to a photo' do
     expect(page).to have_content('Hello, this is comment')
+  end
+
+  scenario 'User can edit a comment' do
+    within('.picture-comments') do
+      click_link(:Edit)
+    end
+    fill_in(:'comment[text]', with: 'Hello, this is edited comment')
+    expect(page).to have_content('Hello, this is edited comment')
+    expect(page).to_not have_content('Hello, this is comment')
   end
 end
