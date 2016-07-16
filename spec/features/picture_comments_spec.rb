ce2802154b56e9482c9629a8d74ca7ce622345d9
Van Le <singularity@vannio.co.uk>
2016-07-16 15:33:53 +0100

    Add test for editing other user comments

--- spec/features/picture_comments_spec.rb
+++ spec/features/picture_comments_spec.rb
@@ -20,4 +20,20 @@ feature 'Picture Comments' do
     expect(page).to have_content('Hello, this is edited comment')
     expect(page).to_not have_content('Hello, this is comment')
   end
+
+  scenario 'User cannot edit another user\'s comment' do
+    user = {
+      email: 'another@example.com',
+      display_name: 'another',
+      password: 'supersecure'
+    }
+    click_link(:'Log Out')
+    sign_up(user)
+    click_link(:Show)
+    within('.picture-comments') do
+      click_link(:Edit)
+    end
+    expect(page).to have_content('You are not the author!')
+
+  end
 end
