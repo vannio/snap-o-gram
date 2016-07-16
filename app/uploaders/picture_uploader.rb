6ca518ba9994d753a6f7389d26162d9fc2904bb4
Van Le <singularity@vannio.co.uk>
2016-07-17 00:04:59 +0100

    Update storage options for production environment

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -6,13 +6,20 @@ class PictureUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  if ENV['RAILS_ENV'] == 'production'
+    storage :fog
+  else
+    storage :file
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{model.id}"
+    if ENV['RAILS_ENV'] == 'production'
+      "snapogram/uploads/#{model.class.to_s.underscore}/#{model.id}"
+    else
+      "uploads/#{model.class.to_s.underscore}/#{model.id}"
+    end
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,11 @@
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    :provider               => 'AWS',
+    :aws_access_key_id      => ENV['AWS_KEY_ID'],
+    :aws_secret_access_key  => ENV['AWS_SECRET'],
+    :region                 => 'eu-west-1'
+  }
+  config.fog_directory  = 'vannio'
+  config.fog_public     = true
+  config.fog_attributes = {'Cache-Control' => 'max-age=315576000'}
+end
